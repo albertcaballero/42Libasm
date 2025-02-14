186a8a84b853517e733cc276bf3496b85265371b
albert <albertcaballerocoll@gmail.com>
2025-02-14 15:38:50 +0100

    started strcpy

--- main.c
+++ main.c
@@ -3,12 +3,24 @@
 #include <string.h>
 
 extern int ft_strlen(char *c);
+extern char *ft_strcpy(char *c, char *s);
 
 int main(void){
 	char str[] = "gasdfasdfasd";
 
 
-	int i = strlen(str);
-    int j = ft_strlen(str);
-    printf("\tORIGINAL: %d || ASM: %d\n", i, j);
+	// int i = strlen(str);
+    // int j = ft_strlen(str);
+    // printf("\tORIGINAL: %d || ASM: %d\n", i, j);
+
+
+
+	char tmp[100];
+    char tmp1[100];
+    char *ex;
+    char *ex1;
+    ex = strcpy(tmp, "Holiwiaaa");
+    printf("orig\tTMP: %s || EX: %s\n", tmp, ex);
+    ex1 = ft_strcpy(tmp1, "Holiwiaaa");
+    printf("mine\tTMP: %s || EX: %s\n", tmp1, ex1);
 }
--- src/ft_strcpy.s
+++ src/ft_strcpy.s
@@ -2,13 +2,15 @@ section .text
     global ft_strcpy
 
 ft_strcpy:
-    xor rax, rax
+    xor rax, rax 
 ;rdi = dst, rsi = src
 loop:
+	mov rdi + rax, [rsi + rax]
     cmp byte [rsi + rax], 0
     je finish
     inc rax
 	jmp loop
 
 finish:
+	mov rax, rdi
     ret
